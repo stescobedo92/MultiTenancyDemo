7563dfbf8dcff4db8a4a484358205beb05d0360f
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-06-26 10:43:32 -0600

    Add HomeIndexViewModel

---
+++ MultiTenancyDemo/Models/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MultiTenancyDemo.Entities;
+
+namespace MultiTenancyDemo.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+        public IEnumerable<Countries> Countries { get; set; } = new List<Countries>();
+
+    }
+}
