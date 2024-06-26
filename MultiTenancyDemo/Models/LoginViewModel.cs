10364964d23f27f2a01e8c91f09afb7a280fcfd8
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-06-26 09:44:33 -0600

    Adding view models and constants

---
+++ MultiTenancyDemo/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiTenancyDemo.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field must be a valid email")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Remind me")]
+        public bool RemindMe { get; set; }
+    }
+}
---
+++ MultiTenancyDemo/Services/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiTenancyDemo.Services
+{
+    public class Constants
+    {
+        public const string CLAIM_TENANT_ID = "tenantId";
+    }
+}
