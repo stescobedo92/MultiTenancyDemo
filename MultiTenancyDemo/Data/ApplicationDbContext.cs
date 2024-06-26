f1c9814310e31d1180578ab8401ad259b65e6b5a
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-06-26 07:43:37 -0600

    Add GlobalFilter for all entities with tenats

--- MultiTenancyDemo/Data/ApplicationDbContext.cs
+++ MultiTenancyDemo/Data/ApplicationDbContext.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Http;
+using MultiTenancyDemo.Entities;
 using MultiTenancyDemo.Services;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace MultiTenancyDemo.Data
 {
@@ -10,6 +14,37 @@ namespace MultiTenancyDemo.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ITenantService tenantService): base(options)
         {
             _tenantId = tenantService.GetTenantId();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                Type clrType = entity.ClrType;
+
+                if (typeof(ITenatEntity).IsAssignableFrom(clrType))
+                {
+                    //build filter
+                    var method = typeof(ApplicationDbContext).GetMethod(nameof(BuildGlobalTenantFilter), BindingFlags.Static | BindingFlags.Public)?.MakeGenericMethod(clrType);
+
+                    var filter = method?.Invoke(null, new object[] { this });
+                    entity.SetQueryFilter(queryFilter: filter as LambdaExpression);
+                    entity.AddIndex(property: entity.FindProperty(nameof(ITenatEntity.TenatId))!);
+                }
+                else
+                {
+                    throw new Exception($"The entity {entity} has not been marked with ITennatEntiy or ICommonEntity");
+                }
+            }
+        }
+
+        private static LambdaExpression BuildGlobalTenantFilter<TEntity>(ApplicationDbContext applicationDbContext) where TEntity : class, ITenatEntity 
+        {
+            Expression<Func<TEntity, bool>> filter = tenatEntity => tenatEntity.TenatId.Equals(applicationDbContext._tenantId);
+
+            return filter;
         }
     }
 }
